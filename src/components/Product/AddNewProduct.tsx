6546c6e13df3601ee9a671b102a9df2c415f54f7
devsv22 <dev.sv2022@gmail.com>
2022-06-19 19:21:21 -0600

    feat: product page added

--- src/components/Product/AddNewProduct.tsx
+++ src/components/Product/AddNewProduct.tsx
@@ -1,5 +1,68 @@
+import { useState } from 'react';
+import Button from 'components/Button';
+import Input from 'components/Input';
+import styled from 'styled-components';
+import { axiosApi } from 'utils/axios';
+
+const ProductContainerForm = styled.div`
+  input {
+    margin-bottom: 10px;
+  }
+`;
+
+const ProductContainerActions = styled.div`
+  margin-top: 10px;
+`;
+
 const AddNewProduct = () => {
-  return <div>Product page!</div>;
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [price, setPrice] = useState(0);
+
+  const onProductSubmit = async () => {
+    if (name.length > 0 && description.length > 0 && price > 0) {
+      const response = await axiosApi.post('/products', {
+        name,
+        description,
+        price,
+      });
+
+      if (response.status === 201) {
+        alert('Product added successfully');
+        setName('');
+        setDescription('');
+        setPrice(0);
+      }
+    }
+  };
+
+  return (
+    <>
+      <ProductContainerForm>
+        <Input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(ev) => setName(ev.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Description"
+          value={description}
+          onChange={(ev) => setDescription(ev.target.value)}
+        />
+        <Input
+          type="number"
+          placeholder="Price"
+          value={price}
+          onChange={(ev) => setPrice(+ev.target.value)}
+        />
+      </ProductContainerForm>
+      <ProductContainerActions>
+        <Button onClick={onProductSubmit}>Guardar</Button>
+      </ProductContainerActions>
+    </>
+  );
 };
 
 export default AddNewProduct;
